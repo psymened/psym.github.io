6936b5ba1a952940408a68430a73bbcb208df444
psy <30380547+mkousaka@users.noreply.github.com>
2018-01-10 15:40:29 +0900

    web

--- js/css.js
+++
@@ -1,6 +0,0 @@
-if (navigator.userAgent.indexOf('Android') > 0 || navigator.userAgent.indexOf('iPhone') > 0) {
-  document.write('<link rel="stylesheet" href="css/m.styles.css" type="text/css">');
-}
-else {
-  document.write('<link rel="stylesheet" href="css/styles.css" type="text/css">');
-}
