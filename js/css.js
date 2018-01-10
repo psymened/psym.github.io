3a0165938b240bf8460ae951d6b920b2e5333a4e
psy <30380547+mkousaka@users.noreply.github.com>
2018-01-10 14:04:20 +0900

    web

--- js/css.js
+++ js/css.js
@@ -1,6 +1,6 @@
 if (navigator.userAgent.indexOf('Android') > 0 || navigator.userAgent.indexOf('iPhone') > 0) {
-  document.write('<link rel="stylesheet" href="m.styles.css" type="text/css">');
+  document.write('<link rel="stylesheet" href="css/m.styles.css" type="text/css">');
 }
 else {
-  document.write('<link rel="stylesheet" href="styles.css" type="text/css">');
+  document.write('<link rel="stylesheet" href="css/styles.css" type="text/css">');
 }
